514816a5a3e2bd5b4d0d9aaa8ed1ac1faa66e124
Eluinhost <graham_howden1@yahoo.co.uk>
2016-04-19 21:22:49 +0100

    add serialization to UblEntry class

--- src/main/kotlin/Entry.kt
+++ src/main/kotlin/Entry.kt
@@ -1,10 +1,13 @@
 package gg.uhc.ubl
 
+import org.bukkit.configuration.serialization.ConfigurationSerialization
 import org.bukkit.plugin.java.JavaPlugin
 
 class Entry() : JavaPlugin() {
     override fun onEnable() {
         config.options().copyDefaults(true)
         saveConfig()
+
+        ConfigurationSerialization.registerClass(UblEntry::class.java)
     }
 }
