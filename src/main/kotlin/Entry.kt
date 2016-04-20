5194b0bb9bcb10c6085a6d48a74f12ddf93624ee
Eluinhost <graham_howden1@yahoo.co.uk>
2016-04-20 20:32:09 +0100

    read from config + setup system

--- src/main/kotlin/Entry.kt
+++ src/main/kotlin/Entry.kt
@@ -1,10 +1,44 @@
 package gg.uhc.ubl
 
+import gg.uhc.ubl.parser.BackupsUblParser
+import gg.uhc.ubl.parser.GoogleSpreadsheetUblParser
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+import java.text.SimpleDateFormat
 
 class Entry() : JavaPlugin() {
     override fun onEnable() {
         config.options().copyDefaults(true)
         saveConfig()
+
+        val names = GoogleSpreadsheetUblParser.GoogleSpreadSheetColumnNames(
+            caseUrl = config.getString("column names.caseUrl"),
+            expiryDate = config.getString("column names.expiryDate"),
+            dateBanned = config.getString("column names.dateBanned"),
+            ign = config.getString("column names.ign"),
+            lengthOfBan = config.getString("column names.lengthOfBan"),
+            reason = config.getString("column names.reason"),
+            uuid = config.getString("column names.uuid")
+        )
+
+        val handler = UblHandler(
+            plugin = this,
+            liveParser = GoogleSpreadsheetUblParser(
+                documentId = config.getString("google spreadsheet id"),
+                worksheetId = config.getString("worksheet id"),
+                dateFormat = SimpleDateFormat("MMMMMddyyyy"),
+                fieldNames = names,
+                logger = logger
+            ),
+            backupsParser = BackupsUblParser(File(dataFolder, "ubl-backup.yml")),
+            notInitializedMessage = config.getString("waiting first load message"),
+            kickMessage = config.getString("banned message"),
+            period = config.getInt("auto refresh minutes") * 60 * 20L
+        )
+
+        handler.loadBackup()
+        handler.start()
+
+        server.pluginManager.registerEvents(handler, this)
     }
 }
